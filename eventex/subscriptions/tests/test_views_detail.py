229adeda3503b89c44ca3036765b5220135520d9
Giovani J. Fontana <fontanagiovani@gmail.com>
2013-08-04 18:41:15 -0400

    Refatorando testes e templates para retirada de urls hardcoded

--- eventex/subscriptions/tests/test_views_detail.py
+++ eventex/subscriptions/tests/test_views_detail.py
@@ -1,6 +1,7 @@
 #coding: utf-8
 from django.test import TestCase
 from eventex.subscriptions.models import Subscription
+from django.core.urlresolvers import reverse as r
 
 class DetailTest(TestCase):
     def setUp(self):
@@ -8,7 +9,7 @@ class DetailTest(TestCase):
                                         cpf='12345678901',
                                         email='fontanagiovani@gmail.com',
                                         phone='65-96193650')
-        self.resp = self.client.get('/inscricao/%d/' % s.pk)
+        self.resp = self.client.get(r('subscriptions:detail', args=[s.pk]))
 
     def test_get(self):
         'GET /inscricao/1/ should return status 200.'
@@ -31,5 +32,5 @@ class DetailTest(TestCase):
 
 class DetailNotFound(TestCase):
     def test_not_found(self):
-        response = self.client.get('/inscricao/0/')
+        response = self.client.get(r('subscriptions:detail', args=[0]))
         self.assertEqual(404, response.status_code)
--- eventex/subscriptions/tests/test_views_subscribe.py
+++ eventex/subscriptions/tests/test_views_subscribe.py
@@ -2,12 +2,13 @@
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
+from django.core.urlresolvers import reverse as r
 
 
 class SubscribeTest(TestCase):
 
     def setUp(self):
-        self.resp = self.client.get('/inscricao/')
+        self.resp = self.client.get(r('subscriptions:subscribe'))
 
     def test_get(self):
         'GET /inscricao/ must return status code 200.'
@@ -38,7 +39,7 @@ class SubscriblePostTest(TestCase):
     def setUp(self):
         data = dict(name='Giovani Jose Fontana', cpf='12345678901',
                     email='fontanagiovani@gmail.com', phone='65-96193650')
-        self.resp = self.client.post('/inscricao/', data)
+        self.resp = self.client.post(r('subscriptions:subscribe'), data)
 
     def test_post(self):
         'Valid POST should redirect to /inscricao/1/'
@@ -57,7 +58,7 @@ class SubscribeInvalidPostTest(TestCase):
             email='fontanagiovani@gmail.com',
             phone='65-96193650'
         )
-        self.resp = self.client.post('/inscricao/', data)
+        self.resp = self.client.post(r('subscriptions:subscribe'), data)
 
     def test_post(self):
         'Invalid POST should not redirect.'
