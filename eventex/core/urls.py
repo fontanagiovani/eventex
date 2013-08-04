3f33f2d46a2f7f1265256e1cf127b4097e27401d
Giovani J. Fontana <fontanagiovani@gmail.com>
2013-08-04 17:14:10 -0400

    Refatorando rotas do urls.py

---
+++ eventex/core/urls.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('eventex.core.views',
+                       url(r'^$', 'homepage', name='homepage'),
+                       )
--- eventex/urls.py
+++ eventex/urls.py
@@ -1,28 +1,18 @@
-#coding utf-8
+# coding utf-8
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'eventex.views.home', name='home'),
-    # url(r'^eventex/', include('eventex.foo.urls')),
+                       url(r'^inscricao/', include('eventex.subscriptions.urls'), name='subscriptions'),
+                       url(r'', include('eventex.core.urls'), name='core'),
+                       )
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'eventex.core.views.homepage', name='homepage'),
-    url(r'^inscricao/$', 'eventex.subscriptions.views.subscribe', name='subscribe'),
-    url(r'^inscricao/(\d+)/$', 'eventex.subscriptions.views.detail', name='detail'),
-)
-
-from django.conf import settings
 
 urlpatterns += patterns('django.views.static',
-    url(r'^static/(?P<path>.*)$', 'serve',
-        {'document_root': settings.STATIC_ROOT}),
-)
+                        url(r'^static/(?P<path>.*)$', 'serve',
+                            {'document_root': settings.STATIC_ROOT}),
+                        )
