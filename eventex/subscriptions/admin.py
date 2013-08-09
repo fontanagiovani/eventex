7085d528ccff07fb1e96d68822c7b34014f04820
Giovani J. Fontana <fontanagiovani@gmail.com>
2013-08-09 15:29:53 -0400

    Inclusao do mock e adicao da action mark_as_paid

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -17,4 +17,9 @@ class SubscriptionAdmin(admin.ModelAdmin):
     subscribed_today.short_description = _(u'Inscrito hoje?')
     subscribed_today.boolean = True
 
+    actions = ['mark_as_paid']
+
+    def mark_as_paid(self, request, queryset):
+        pass
+
 admin.site.register(Subscription, SubscriptionAdmin)
